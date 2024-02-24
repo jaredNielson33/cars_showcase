2ded489a6d5f0cd50bd3c64ea071bc958c531c13
Jared Nielson <jarednielson@Jareds-MacBook-Pro-2.local>
2024-02-24 13:19:36 -0700

    initial commit

---
+++ utils/index.ts
@@ -0,0 +1,51 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, fuel, limit } = filters;
+  const headers = {
+    'X-RapidAPI-Key': 'b2ea499c71mshb91306d9ec2d118p1ee613jsn7ec5188fabee',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+  
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&model=${model}&year=${year}&fuel_type=${fuel}&limit=${limit}`, {headers: headers})
+  const result = await response.json();
+  return result;
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL(`https://cdn.imagin.studio/getimage`)
+  const { make, model, year } = car;
+  
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(type, value);
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathname
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage
+  const mileageRate = city_mpg * mileageFactor;
+
+  // Calculate additional rate based on age of vehicle
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+}
